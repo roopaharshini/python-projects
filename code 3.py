1e8cae6d170175e9a3934a057418e2322da4cf63
roopaharshini <rmunnang@student.gitam.edu>
2026-01-22 15:48:49 +0530

    Create code 3.py

---
+++ code 3.py
@@ -0,0 +1,11 @@
+a = 5
+b = 2
+
+c = a + b * 3
+print(f"Value of c = {c}")
+
+c = a // b * 4   # use // for integer division like C
+print(f"Value of c = {c}")
+
+c = 3 % b * a
+print(f"Value of c = {c}")
