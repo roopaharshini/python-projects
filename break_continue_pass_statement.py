186a6b2f0440e869d9217ccf18f008daff95d1b3
roopaharshini <rmunnang@student.gitam.edu>
2026-01-28 11:22:42 +0530

    Create break_continue_pass_statement.py

---
+++ break_continue_pass_statement.py
@@ -0,0 +1,14 @@
+for i in range(1, 11):
+    if i == 3:
+        pass       
+        print("Pass at", i)
+
+    elif i == 5:
+        continue   
+
+    elif i == 8:
+        break      
+
+    print("Number:", i)
+
+print("Loop finished")
