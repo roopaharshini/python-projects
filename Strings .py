3a1edd3bf8e2586d3df144c1242c57f16489c79d
roopaharshini <rmunnang@student.gitam.edu>
2026-02-05 15:34:14 +0530

    Create Strings .py

---
+++ Strings .py
@@ -0,0 +1,9 @@
+s = "GeeksforGeeks"
+print(s[0])   # first character
+print(s[4])   # 5th character
+print(s[-10])   # 3rd character
+print(s[-5])    # 5th character from end
+print(s[1:4])    # characters from index 1 to 3
+print(s[:3])     # from start to index 2
+print(s[3:])     # from index 3 to end
+print(s[::-1])   # reverse string
