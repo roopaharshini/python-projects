6b684da3f29aa58fecc6a3cb787c57ec5ee36b4d
roopaharshini <rmunnang@student.gitam.edu>
2026-02-12 15:33:56 +0530

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,46 @@
+# List
+li = ["apple", "banana", "cherry"]
+for x in li:
+    print(x)
+
+# Tuple
+tup = ("red", "green", "blue")
+for x in tup:
+    print(x)
+
+# String
+s = "python"
+for x in s:
+    print(x)
+
+# Dictionary
+d = {'a': 100, 'b': 200}
+for x in d:
+    print("%s  %d" % (x, d[x]))
+
+# Set
+set1 = {5, 15, 25}
+for x in set1:
+    print(x)
+
+
+
+
+Output:
+apple
+banana
+cherry
+red
+green
+blue
+p
+y
+t
+h
+o
+n
+a  100
+b  200
+5
+15
+25
